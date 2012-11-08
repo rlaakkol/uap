bc6ae34b3196843edd78e637de8a1c9fa94ed831
Riku Lääkkölä <riku.laakkola@iki.fi>
2012-11-08 22:09:54 +0200

    first implementations

--- trans/trans.c
+++ trans/trans.c
@@ -5,6 +5,7 @@
 static char 	codes[47][8] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "..--..", "-....-", "---...", "-.-.-.", "-..-.", "..--.-", "......", ".......", "........"};
 static char 	char_map[47] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '?', '-', ':', ';', '/', '_', '\n', EOF, '#'};
 
+int	sig_flag;
 
 
 int
@@ -12,6 +13,8 @@ main(int argc, char** argv)
 {
 	int 	pid;
 	FILE 	*infile, *outfile;
+	
+	sig_flag = INIT_FLAG;
 
 	if ((pid = fork()) < 0){
 		perror("fork error");
@@ -54,9 +57,52 @@ child_func(char *filename, FILE* infile)
 int
 parent_func(FILE *outfile, int cpid)
 {
-	
-	while () {
+	char	nextc;
+	char	code[8];
+	int	i;
 
+	while (nextc != EOF) {
+		if (sig_flag == INIT_FLAG) {
+			sig_sleep()
+		}
+		code[0] = sig_to_morse(sig_flag);
+		i = 1;
+		while (sig_flag != MORSE_PAUSE) {
+			kill(cpid, sig_flag);
+			sig_sleep();
+			code[i] = sig_to_morse(sig_flag);
+			i++;
+		}
+		nextc = morse_decode(code);
+		fputc(outfile, nextc);
+		sig_flag = INIT_FLAG;
+		kill(cpid, MORSE_PAUSE);
+	}
+
+
+}
+
+char
+sig_to_morse(int sig)
+{
+	if (sig == MORSE_SHORT) return '.';
+	else if (sig == MORSE_LONG) return '-';
+	else if (sig == MORSE_PAUSE) return '\0';
+	else {
+		exit(ERROR);
+	}
+
+int
+sig_sleep(void)
+{
+	struct timespec	sleeptime;
+
+	sleeptime.tv_sec = WAIT;
+	sleeptime.tv_nsec = 0;
+	if (nanosleep(&sleeptime, NULL) != -1) {
+		exit(ERROR);
+	}
+	return 0;
 }
 
 int
@@ -89,3 +135,43 @@ morse_decode(char *code)
 
 	return char_map[i];
 }
+
+int
+sig_char(int pid, char code)
+{
+	char	c;
+	int	i;
+	struct timespec	sleeptime;
+	sleeptime.tv_sec = WAIT;
+	sleeptime.tv_nsec = 0;
+	i = 0;
+	while((c = code[i]) != '\0') {
+		if (i > 0) {
+			if (nanosleep(&sleeptime, NULL) != -1) {
+				exit(ERROR);
+			}
+		}
+		if (c == '.') kill(pid, MORSE_SHORT);
+		else if (c == '-') kill(pid, MORSE_LONG);
+		else kill(pid, MORSE_ERR);
+		i++;
+	}
+}
+
+void
+handler(int signo)
+{
+	int	pid;
+
+	pid = getpid();
+
+	if (pid == 0) {
+		/* In child, continue */
+	} else {
+		if (signo == MORSE_SHORT) sig_flag = MORSE_SHORT;
+		else if (signo == MORSE_LONG) sig_flag = MORSE_LONG;
+		else if (signo == MORSE_EOC) sig_flag = MORSE_EOC;
+		else sig_flag = MORSE_ERR;
+	}
+}
+		
