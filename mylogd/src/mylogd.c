928e8b3a74ae665c802e6712a7ead1e547d43aa6
Riku Lääkkölä <riku.laakkola@iki.fi>
2012-12-14 09:52:58 +0200

    more

--- mylogd/src/mylogd.c
+++ mylogd/src/mylogd.c
@@ -1,13 +1,54 @@
 
 #define FIFOPATH "/tmp/mylog.main"
+#define FIFOFMT "tmp/mylog.%d"
 #define FIFOPERMS (S_IRWXU | S_IWGRP| S_IWOTH)
+#define MAXLEN 256
 
 FILE 	*logfile;
 
-void
-logger(void *pid)
+int
+output_msg(int readlen, char *buf, char *rem)
 {
+	int 	i;
 
+	i = 0;
+
+	while (i < readlen && buf[i] != '\0')
+
+}
+void
+logger(void *caller_pid)
+{
+	int 	pid, logfd, out;
+	char 	*fifo, *buf, *rem;
+	fd_set 	readfds;
+
+	fifo = malloc(30);
+	buf = malloc(MAXLEN);
+	rem = malloc(MAXLEN);
+
+	pid = (int) caller_pid;
+	snprintf(fifo, FIFOFMT, pid);
+	if (mkfifo(fifo, FIFOPERMS) != 0) {
+
+	}
+	FD_ZERO(&readfds);
+	for (;;) {
+		FD_SET(logfd, readfds);
+		select(logfd + 1, &readfds, NULL, NULL, NULL);
+		readlen = read(logfd, buf, BUFLEN);
+		while ((out = output_msg(readlen, buf, rem)) > 0) {
+			readlen -= out;
+		}
+		if (out == -1) break;
+
+
+	}
+
+
+	free(fifo);
+	free(buf);
+	pthread_exit(NULL);
 }
 
 int
